6959f92fab7b30275e153600a7a905b2769a5fa9
shavit <shavit@users.noreply.github.com>
2019-01-19 07:08:33 -0500

    Generate gradient

---
+++ gradient.py
@@ -0,0 +1,25 @@
+
+def write(name, data):
+    with open('out/{}'.format(name), 'w') as f:
+        f.write(data)
+
+def draw():
+    u = 600
+    v = 400
+    data = 'P3\n{} {}\n{}\n'.format(u, v, 255)
+
+    for j in range(v, 0, -1):
+        for i in range(0, u):
+            red = float(i) / u
+            green = float(j) / v
+            blue = 1.618034
+            r = int(red * 255.99)
+            g = int(green * 255.99)
+            b = int(blue * 255.99)
+            data += '{} {} {}\n'.format(r, g, b)
+
+    write('gradient.pmm', data)
+
+
+if __name__ == '__main__':
+    draw()
